5f13809c3057f99b27a85b2b5f46f929c4f8a040
voidrizoma <milhadasdepapel@gmail.com>
2023-04-14 20:58:24 -0600

    initial blog

---
+++ src/pages/about.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <div>      <article class="set-width">
+    <p>
+      Hi!, my name is <span class="name-alias">Karen Molina</span>, also can
+      find me as <span class="name-alias">Void Rizoma</span>. Currently I’m
+      living in Mexico State. I have two cats
+      <span role="img" aria-label="cat">🐈 </span>Tito and
+      <span role="img" aria-label="cat">🐈 </span> Simon. I love
+      <span role="img" aria-label="chips">🍟</span> chips,
+      <span role="img" aria-label="bat">🦇 </span>Batman and<span
+        role="img"
+        aria-label="spider"
+        >🕷️
+      </span>
+      Spiderman.
+    </p>
+    <p>
+      When I was a child, I remember being into sciences. I used to want to
+      be a biologist, so I studied
+      <span class="name-interest">Biology </span>
+      <span role="img" aria-label="biology">🧬 </span> at UNAM. But when I
+      was study biology, I discovered a lot of authors that wrote about
+      humanities, philosophy and anthropology. In consequence, I dropped
+      Biology and started to study
+      <span class="name-interest">Social Anthropology</span> at UAM. Since I
+      was a child always have access to technology, I grow up with
+      <span class="name-interest">Internet </span>,
+      <span class="name-interest">computers </span> and
+      <span class="name-interest">videogames </span> next to me. So in the
+      college, one day I woke up with curiosity about
+      <span class="name-interest">Free Software</span> , and I using it
+      since there, so I wrote my research about it. Then I discovered many
+      FS communities and realized many of the contributors where engineering
+      students or graduated engineers. It was there when I started to learn
+      about coding. My first approach was with
+      <span class="name-interest">Arduino </span>, since there, I’m really
+      into <span class="name-interest">digital culture </span>issues. I’m
+      teacher on many workshops <span class="name-interest">STEAM </span> 🔬
+      that I develop. I’m contributor to many foundations, schools and
+      organizations. Since 2019 I’m
+      <span class="name-interest">Frontend developer </span>. Currently,
+      <span role="img" aria-label="woman"> 👩‍💻 </span>I’m a frontend
+      developer at FluxQR, a fintech mexican startup. Sometimes I write
+      stuffs on
+      <a
+        href="https://dev.to/voidrizoma"
+        target="_blank"
+        class="name-interest"
+        >dev.to</a
+      >
+    </p>
+  </article></div>
+  )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link } from 'gatsby'
+import '../styles/index.css'
+
+export default function index() {
+  return (
+    <div>
+    <h1>Karen Molina</h1>
+    <h3>Frontend Developer <span role="img" aria-label="woman"> 👩‍💻 </span></h3> 
+
+  </div>
+  )
+}
