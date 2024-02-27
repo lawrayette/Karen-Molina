3bd2d8c302fb4dbbf7b2b97b5eec7a6a2d90cf3f
voidrizoma <voidrizoma@Karens-Mac-mini.local>
2024-02-26 23:02:59 -0600

    ..

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://voidrizoma.dev',
   integrations: [mdx(), sitemap()],
+  output: "server",
   adapter: vercel()
 });
