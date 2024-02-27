8e5e39c8136313582fb565b0e70ef5486d925dc0
voidrizoma <voidrizoma@Karens-Mac-mini.local>
2024-02-26 23:09:34 -0600

    ..

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   site: 'https://voidrizoma.dev',
   integrations: [mdx(), sitemap()],
-  output: "server",
+  output: "hybrid",
   adapter: vercel()
 });
